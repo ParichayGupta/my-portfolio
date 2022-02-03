de6502aa055d59a5d78cc99083c186036e676c8d
ParichayGupta <parichaygupta93@gmail.com>
2022-02-03 17:08:42 +0530

    layout changes

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-let height = window.innerHeight;
-let width = window.innerWidth;
+// let height = window.innerHeight;
+// let width = window.innerWidth;
 
-console.log("height: " + height + " widht: " + width)
+// console.log("height: " + height + " widht: " + width)
 
-let navItems = $(".navbar-light .navbar-nav .nav-link");
+// let navItems = $(".navbar-light .navbar-nav .nav-link");
 
-$(window).scroll(function () {
-    let distance = $(window).scrollTop();
+// $(window).scroll(function () {
+//     let distance = $(window).scrollTop();
 
-    $('.page-section').each(function (i) {
+//     $('.page-section').each(function (i) {
 
-        if ($(this).position().top
-            <= distance + 100) {
+//         if ($(this).position().top
+//             <= distance + 100) {
 
-            $('.navbar-nav a.active')
-                .removeClass('active');
+//             $('.navbar-nav a.active')
+//                 .removeClass('active');
 
-            $('.navbar-nav a').eq(i)
-                .addClass('active');
-        }
-    });
-}).scroll();
+//             $('.navbar-nav a').eq(i)
+//                 .addClass('active');
+//         }
+//     });
+// }).scroll();
