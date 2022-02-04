b29e453a40d41aa16cf5a26711ebf0e6c588ca70
ParichayGupta <parichaygupta93@gmail.com>
2022-02-04 18:33:20 +0530

    added scroll indicator

--- script.js
+++ script.js
@@ -21,3 +21,14 @@
 //         }
 //     });
 // }).scroll();
+
+
+
+window.onscroll = function() { scrollIndecator() };
+
+function scrollIndecator() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
